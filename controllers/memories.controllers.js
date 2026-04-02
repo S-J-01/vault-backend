420fb7eda17e020c9f5ec9cbbdd2f8b29e1b3a94
Shreyas Jaltare <shreyasjaltare@gmail.com>
2026-04-02 23:40:57 +0530

    feat: add fetchAllmemories controller function

--- controllers/memories.controllers.js
+++ controllers/memories.controllers.js
@@ -1,6 +1,14 @@
+import { Memory } from "../models/memories.models.js";
 export const createMemory = (req, res) => {};
 
-export const fetchAllMemories = (req, res) => {};
+export const fetchAllMemories = async (req, res) => {
+  try {
+    const memoriesList = await Memory.find({});
+    res.status(200).json({ AllMemories: memoriesList });
+  } catch {
+    res.status(500).json({ message: "failed to get memories" });
+  }
+};
 
 export const findMemoriesByTag = (req, res) => {};
 
